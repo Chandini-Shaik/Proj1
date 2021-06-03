1e4118eee74c1884599ae885e5175b937293d67b
chandini shaik <Chandinisheikh18@gmail.com>
2021-06-03 15:43:31 +0530

    Add project files.

---
+++ IService1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfRestPost1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        //[WebInvoke(UriTemplate = "CustomersJson", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        //void PostDataUsingDataContract(Customer c);
+        [OperationContract]
+        [WebInvoke(UriTemplate = "CustomersJson", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        void PostDataUsingDataContract(Customers c);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "CustomersXml", ResponseFormat = WebMessageFormat.Xml, RequestFormat = WebMessageFormat.Xml)]
+        List<Customers> GetDataUsingDataContractXML();
+
+    }
+
+        // Use a data contract as illustrated in the sample below to add composite types to service operations.
+        [DataContract]
+        public class Customers
+        {
+            string custName;
+            [DataMember]
+            public string CustName
+            {
+                get { return custName; }
+                set
+                {
+                    custName =value;
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+    
---
+++ Service1.svc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfRestPost1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        static List<Customers> custlist = new List<Customers>();
+
+        public List<Customers> GetDataUsingDataContractXML()
+        {
+            //throw new NotImplementedException();
+            return custlist;
+        }
+
+        //public List<IService1.Customers> GetDataUsingDataContractXML()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public void PostDataUsingDataContract(Customers c)
+        {
+            Customers c1 = new Customers();
+            c1.CustName = c.CustName;
+            custlist.Add(c1);
+            //throw new NotImplementedException();
+        }
+
+        //public void PostDataUsingDataContract(IService1.Customers c)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
+        
